a834d7702e2a4cabf28e0a74d810d45e21996cff
Loc Luu <lluu3790@gmail.com>
2021-02-14 00:24:01 +0000

    comment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Hello World!\n";
-  
+  cout << "Hello users,\n ";
+  //option  new user or old user
+  //new user site
+    // check if they exists by username/email
+    // store username/email with uniqe Id
+
+  //old user login site
+    // check if validate
+    // return user Id
+
+  // login site with specific Id
+  // input to do
+    // compare with the existing one
+
+  // search engine
+    // return either by day or name of task
+    
 }
